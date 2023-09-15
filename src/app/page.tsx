02ae2130258879ac9b2c0b5eafe47b49c4629fdb
MELLAHKAIS <mellahkais@MELLAHKAISs-MacBook-Pro.local>
2023-09-15 14:41:17 +0100

    40%completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'remixicon/fonts/remixicon.css'
+import 'boxicons/css/boxicons.min.css'
+import 'public/scss/style.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import Topbar from './components/home/topbar'
+import Hero from './components/home/hero'
+import Header from './components/home/header'
+import Footer from './components/home/footer'
+import Whyus from './components/home/whyus'
+import AboutUs from './components/home/aboutUs'
+import Counts from './components/home/counts'
+import Service from './components/home/service'
+import Appointment from './components/home/appointement'
+import Departement from './components/home/departement'
+import Doctors from './components/home/doctors'
+import Testimonial from './components/home/testimonial'
+import Contact from './components/home/contact'
+import Faq from './components/home/faq'
+import Gallerie from './components/home/gallerie'
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Topbar/>
+      <Header/>
+      <Hero/>
+      <main>
+        <Whyus/>
+        <AboutUs/>
+        <Counts/>
+        <Service/>
+        <Appointment/>
+        <Departement/>
+        <Doctors/>
+        <Faq/>
+        <Testimonial/>
+        <Gallerie/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   )
 }
